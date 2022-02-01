5d255933e3703de3f1a4ba59b80e7e6e9378276f
hanifhefaz <hefaz443@gmail.com>
2022-02-01 15:40:09 +0430

    add home button

--- app/src/main/java/com/example/myapplication/Home.java
+++ app/src/main/java/com/example/myapplication/Home.java
@@ -9,5 +9,14 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), AnActivity.class);
+                view.getContext().startActivity(intent);}
+        });
+
     }
 }
